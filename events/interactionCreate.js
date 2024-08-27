a8ee5b22da28d26d2554cd8cb56262550efbdb0e
= <=>
2024-08-26 22:14:18 -0500

    Trying to fix again. Returns null, so needed to convert to string.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,7 @@ module.exports = {
     event: 'interactionCreate',
     async execute(interaction) {
         if (interaction.isCommand()) {
-            console.log(`(Guild: ${interaction.guild.id})\t Command triggered: /${interaction.commandName} ${interaction.options.getSubcommand(false).replace('undefined', '')}`);
+            console.log(`(Guild: ${interaction.guild.id})\t Command triggered: /${interaction.commandName} ${toString(interaction.options.getSubcommand(false)).replace('null', '')}`);
             const command = interaction.client.commands.get(interaction.commandName);
 
 			if (!command) { // Command was not found in the files
